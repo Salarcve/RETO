a17790a2ce35b355dbac44898c3420abc20ab08a
Juan <juan.gallegomesa@gmail.com>
2020-09-08 20:15:24 -0500

    reto laboral

---
+++ reto.js
@@ -0,0 +1,6 @@
+//RETO DEL FIZZ BUZZ (200 numeros enteros)
+
+/*Escribe, un programa que muestre en pantalla los números del 1 al 200, 
+sustituyendo los múltiplos de 3 por el palabro “Fizz” y, a su vez, los múltiplos 
+de 5 por “Buzz”. Para los números que, al tiempo, son múltiplos de 3 y 5, utiliza el combinado 
+“FizzBuzz*/
